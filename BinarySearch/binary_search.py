f9c869361094d0869ac43c5a3606f8c05992f0d9
Shadman Sakeeb <shadman.sakeeb.03@gmail.com>
2023-07-31 02:16:55 +0600

    refactored some variable names

--- BinarySearch/binary_search.py
+++ BinarySearch/binary_search.py
@@ -1,12 +1,12 @@
 # Assuming that the seq is sorted in descending order.
 def binary_search(seq, query):
-    low = 0
-    high = len(seq) - 1
-    while low <= high:
-        mid = (low + high) // 2
+    left = 0
+    right = len(seq) - 1
+    while left <= right:
+        mid = (left + right) // 2
         if seq[mid] == query:
             return mid
         elif seq[mid] < query:
-            high = mid - 1
+            right = mid - 1
         elif seq[mid] > query:
-            low = mid + 1
+            left = mid + 1
